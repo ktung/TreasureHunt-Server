67a6f44285ddb9e4a35aa49d5cc9343c044544e2
Pascal Tung <tung.pascal@etu.unice.fr>
2017-01-19 10:47:05 +0100

    send areas

--- socket/areas.js
+++ socket/areas.js
@@ -1,10 +1,12 @@
+var mongoose = require('mongoose');
+
 exports = module.exports = function(socket){
     socket.on('areasRequest', function () {
         mongoose.model('Area').find({}, function (err, areas) {
             if (err) {
                 return console.error(err);
             } else {
-                socket.emit('areas', res.json(areas));
+                socket.emit('areas', areas);
             }
         });
     });
