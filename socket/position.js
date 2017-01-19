413a169ed585e0801e72bb100e0b5a485f3c7610
Pascal Tung <tung.pascal@etu.unice.fr>
2017-01-19 10:35:03 +0100

    move marker position

--- socket/position.js
+++ socket/position.js
@@ -1,10 +1,10 @@
 exports = module.exports = function(socket){
     socket.on('sendPosition', function (data) {
         console.log(data);
-        var lat = data.position.coords.latitude;
-        var long = data.position.coords.longitude;
+        var lat = data.latitude;
+        var long = data.longitude;
 
-        var position = {latitude: lat, longitude: long};
-        socket.to(global.positionServerId).emit("userPosition", position);
+        var data = {id: 1, position: {latitude: lat, longitude: long}};
+        socket.to(global.positionServerId).emit("userPosition", data);
     });
 };
