b64bed0e7dcc0d35b500dba35d4a17f825ea8a5d
Pascal Tung <tung.pascal@etu.unice.fr>
2017-01-26 11:00:16 +0100

    fix remote db

--- models/db.js
+++ models/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/treasurehunt');
-// mongoose.connect('mongodb://jacob:motdepassedejacob@ds129179.mlab.com:29179/treasurehunt');
+// mongoose.connect('mongodb://localhost/treasurehunt');
+mongoose.connect('mongodb://jacob:motdepassedejacob@ds129179.mlab.com:29179/treasurehunt');
