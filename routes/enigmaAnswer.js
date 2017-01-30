0bed4cec3add7d2bc2d112221faa9a78a42af661
Loïc Potages <loic.potages@etu.unice.fr>
2017-01-30 12:03:52 +0100

    Route for Mapping enigma and answers

--- routes/enigmaAnswer.js
+++ routes/enigmaAnswer.js
@@ -9,58 +9,92 @@ app = express(); //mongo connection
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.route('/enigmaAnswer')
-    .get(function(req, res) {
+router.route('enigmaAnswer')
+    .get(function(req, res){
         res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
 
-        var p1 = new Promise(function(resolve, reject) {
-            mongoose.model('EnigmaAnswer').find({validated:false}, function (err, enigmasAnswer) {
-                if (err) {
-                    return console.error(err);
-                } else {
-                    var p2 = new Promise(function(resolve, reject) {
-                        var response = [];
+        mongoose.model('Enigma').find({}, function (err, enigmasFound){
+            if (err) {
+                console.log("Could not retrieve enigmas enigmaAnswerRoute")
+            } else{
+                var enigmas = enigmasFound; // Tableau avec toutes les enigmes
+                var tabLength = enigmasFound.length;
 
-                        var i = 0;
-                            console.log(i)
-                            console.log(enigmasAnswer.length)
-                        async.whilst(function() {
-                            i < enigmasAnswer.length
-                        }, function(next) {
-                            console.log(i)
-                            var answer = enigmasAnswer[i];
-                            if (null != answer.enigmaId) {
-                                    response.push("lfsj")
-                                mongoose.model('Enigma')
-                                .findById(answer.enigmaId, function(err, enigma) {
-                                    console.log(response)
+                var finalTab = [];
+
+                for(var i= 0; i < tabLength; i++){
+                    mongoose.model('EnigmaAnswer').find({_id : enigmas[i]._id}, function(err,answersFound){
+                        if (err){
+                            console.log("Could not find enigmaAnswer for answer " + i);
+                        } else {
+                            finalTab.push({enigma : enigmas[i], answers: answersFound});;
+                            if(finalTab.length == tabLength){
+                                res.format({
+                                    //JSON response will show all blobs in JSON format
+                                    json: function(){
+                                        res.json(finalTab);
+                                    }
                                 });
                             }
-
-                            ++i;
-                            next();
-                        }, function(err) {
-                            resolve(response);
-                        })
-                    });
-                    p2.then(function(data) {
-                        resolve(data);
-                    }).catch(function(err) {
-                        console.log("Error promise : "+ err)
+                        }
                     })
                 }
-
-            });
-        });
-        p1.then(function(data) {
-            res.format({
-                json: function(){
-                    res.json(data);
-                }
-            });
-        }).catch(function() {
-            console.log("Erreur promise")
-        });
+            }
+        })
     });
 
+/*router.route('/enigmaAnswer')
+ .get(function(req, res) {
+ res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+
+ var p1 = new Promise(function(resolve, reject) {
+ mongoose.model('EnigmaAnswer').find({validated:false}, function (err, enigmasAnswer) {
+ if (err) {
+ return console.error(err);
+ } else {
+ var p2 = new Promise(function(resolve, reject) {
+ var response = [];
+
+ var i = 0;
+ console.log(i)
+ console.log(enigmasAnswer.length)
+ async.whilst(function() {
+ i < enigmasAnswer.length
+ }, function(next) {
+ console.log(i)
+ var answer = enigmasAnswer[i];
+ if (null != answer.enigmaId) {
+ response.push("lfsj")
+ mongoose.model('Enigma')
+ .findById(answer.enigmaId, function(err, enigma) {
+ console.log(response)
+ });
+ }
+
+ ++i;
+ next();
+ }, function(err) {
+ resolve(response);
+ })
+ });
+ p2.then(function(data) {
+ resolve(data);
+ }).catch(function(err) {
+ console.log("Error promise : "+ err)
+ })
+ }
+
+ });
+ });
+ p1.then(function(data) {
+ res.format({
+ json: function(){
+ res.json(data);
+ }
+ });
+ }).catch(function() {
+ console.log("Erreur promise")
+ });
+ });
+ */
 module.exports = router;
